c6f1190d3de5dff4b233f311d03c7f8e6dfa6b67
Tudor Cornea <tudor.cornea@gmail.com>
2015-06-21 23:05:17 +0300

    Updated Sql file with new structure

--- TrafficCollectorServer/sql/traffic_collector.sql
+++ TrafficCollectorServer/sql/traffic_collector.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 3.5.8.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 10, 2015 at 09:22 PM
--- Server version: 5.5.41-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.5
+-- Generation Time: Jun 21, 2015 at 11:03 PM
+-- Server version: 5.5.34-0ubuntu0.13.04.1
+-- PHP Version: 5.4.9-4ubuntu2.4
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -19,23 +19,43 @@ SET time_zone = "+00:00";
 --
 -- Database: `traffic_collector`
 --
-DROP DATABASE IF EXISTS `traffic_collector`;
-CREATE DATABASE IF NOT EXISTS `traffic_collector` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+DROP DATABASE `traffic_collector`;
+CREATE DATABASE `traffic_collector` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `traffic_collector`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `history`
+-- Table structure for table `journey`
 --
 
-DROP TABLE IF EXISTS `history`;
-CREATE TABLE IF NOT EXISTS `history` (
+DROP TABLE IF EXISTS `journey`;
+CREATE TABLE IF NOT EXISTS `journey` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` varchar(512) NOT NULL,
-  `file` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `id_user` int(11) NOT NULL,
+  `journey_name` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_user` (`id_user`),
+  KEY `id_user_2` (`id_user`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `journey_data`
+--
+
+DROP TABLE IF EXISTS `journey_data`;
+CREATE TABLE IF NOT EXISTS `journey_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `journey_id` int(11) NOT NULL,
+  `latitude` float(10,6) NOT NULL,
+  `longitude` float(10,6) NOT NULL,
+  `speed` int(11) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `journey_id` (`journey_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -45,68 +65,83 @@ CREATE TABLE IF NOT EXISTS `history` (
 
 DROP TABLE IF EXISTS `location`;
 CREATE TABLE IF NOT EXISTS `location` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` text NOT NULL,
-  `name` text NOT NULL,
-  `facebook` text NOT NULL,
-  `twitter` text NOT NULL,
-  `lat` float(10,6) NOT NULL,
-  `lng` float(10,6) NOT NULL,
-  `speed` text NOT NULL,
-  `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `stop` int(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `latitude` float(10,6) NOT NULL,
+  `longitude` float(10,6) NOT NULL,
+  `speed` int(11) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `stats`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `stats`;
-CREATE TABLE IF NOT EXISTS `stats` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` text NOT NULL,
-  `server_info` text NOT NULL,
-  `page` text NOT NULL,
-  `type` int(11) NOT NULL COMMENT '0 - other 1 - logged',
-  `login_date` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `username` VARCHAR(100) NOT NULL,
-  `password` text,
-  `firstname` VARCHAR(100) NOT NULL,
-  `lastname` VARCHAR(100) NOT NULL,
-  `phone` VARCHAR(15),
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
   `facebook_token` text,
-  `facebook_expires_in` int,
+  `facebook_expires_in` int(11) NOT NULL,
   `auth_token` text NOT NULL,
-  `auth_expires_in` int
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `auth_expires_in` int(11) NOT NULL,
+  `uuid` varchar(512) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uuid` (`uuid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `friends`
+-- Table structure for table `user_contact`
 --
 
-DROP TABLE IF EXISTS `friends`;
-CREATE TABLE IF NOT EXISTS `friends` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `id_first_user` int,
-  `id_second_user` int
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+DROP TABLE IF EXISTS `user_contact`;
+CREATE TABLE IF NOT EXISTS `user_contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_friend_user` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_user1` (`id_user`),
+  KEY `id_user2` (`id_friend_user`),
+  KEY `id_user1_2` (`id_user`),
+  KEY `id_user2_2` (`id_friend_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `journey`
+--
+ALTER TABLE `journey`
+  ADD CONSTRAINT `journey_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `journey_data`
+--
+ALTER TABLE `journey_data`
+  ADD CONSTRAINT `journey_data_ibfk_1` FOREIGN KEY (`journey_id`) REFERENCES `journey` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `location`
+--
+ALTER TABLE `location`
+  ADD CONSTRAINT `location_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_contact`
+--
+ALTER TABLE `user_contact`
+  ADD CONSTRAINT `user_contact_ibfk_2` FOREIGN KEY (`id_friend_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_contact_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
