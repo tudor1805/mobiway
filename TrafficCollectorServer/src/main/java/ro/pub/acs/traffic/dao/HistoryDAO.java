994e3620a9f8a0a6d88640cd0c9f5f7e0632bcda
Tudor Cornea <tudor.cornea@gmail.com>
2015-06-21 23:39:59 +0300

    Removed HistoryDAO and StatsDAO

--- TrafficCollectorServer/src/main/java/ro/pub/acs/traffic/dao/HistoryDAO.java
+++
@@ -1,9 +0,0 @@
-package ro.pub.acs.traffic.dao;
-
-import java.util.List;
-import ro.pub.acs.traffic.model.History;
-
-public interface HistoryDAO {
-	public List<History> list();
-	public History getHistory(long id);
-}
--- TrafficCollectorServer/src/main/java/ro/pub/acs/traffic/dao/StatsDAO.java
+++
@@ -1,9 +0,0 @@
-package ro.pub.acs.traffic.dao;
-
-import java.util.List;
-import ro.pub.acs.traffic.model.Stats;
-
-public interface StatsDAO {
-	public List<Stats> list();
-	public Stats getStats(long id);
-}
