428e10170ca46cb84f9c75c11109f9470fff8350
Tudor Cornea <tudor.cornea@gmail.com>
2015-07-20 00:04:37 +0300

    Renamed TrafficCollector to Mobiway

---
+++ Mobiway/app/src/main/java/ro/pub/acs/mobiway/general/Constants.java
@@ -0,0 +1,54 @@
+package ro.pub.acs.mobiway.general;
+
+public abstract class Constants {
+    /*   Application Settings   */
+
+    /*   Application Constants   */
+    public static final String LANG_EN = "en";
+    public static final String EMPTY_STRING = "";
+
+
+    /* Google Map */
+    public static final int CAMERA_ZOOM = 14;
+    public static long LOCATION_REQUEST_INTERVAL = 20000;
+    public static long LOCATION_REQUEST_FASTEST_INTERVAL = 10000;
+
+    /*   Application Keys and IDs   */
+    /* URLs for WEB Services */
+    public static final String SERVICES_URL = "http://mobiway.hpc.pub.ro:8082/MobiwayServer/services";
+
+    public static final String URL_LOG_IN = "/authenticate/userpass";
+    public static final String URL_AUTH_FACEBOOK = "/authenticate/facebook";
+    public static final String URL_CREATE_ACCOUNT = "/signup/userpass";
+    public static final String URL_UPDATE_USER = "/user/update";
+    public static final String URL_GET_USER = "/user/getUser";
+    public static final String URL_NEW_JOURNEY = "/location/newJourney";
+    public static final String URL_UPDATE_LOCATION = "/location/update";
+    public static final String URL_GET_FRIENDS_NAMES = "/social/getFriendsNames";
+    public static final String URL_GET_FRIENDS_LOCATIONS = "/social/getFriendsLocations";
+    public static final String URL_GET_USERS_WITH_PHONE = "/social/getUsersWithPhone";
+    public static final String URL_ADD_FRIENDS = "/social/addFriends";
+    public static final String URL_GET_NEARBY_LOCATIONS = "/social/getNearbyLocations";
+    public static final String URL_GET_ROUTE = "/social/getRoute";
+
+    /* SharedPreferences file name */
+    public static final String PREF_NAME = "mobiwayPreferences";
+
+    /* All Shared Preferences Keys */
+    public static final String IS_LOGGED_IN = "isLoggedIn";
+    public static final String KEY_USER_ID = "userId";
+    public static final String KEY_EMAIL = "emailAddress";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_LASTNAME = "lastName";
+    public static final String KEY_FIRSTNAME = "firstName";
+    public static final String KEY_AUTH_TOKEN = "token";
+    public static final String KEY_AUTH_TOKEN_FB = "tokenFB";
+    public static final String KEY_EXPIRES_ON = "expiresOn";
+    public static final String KEY_EXPIRES_IN_FB = "expiresInFB";
+    public static final String KEY_CURRENT_LANGUAGE = "currentLanguage";
+    public static final String KEY_LATITUDE = "latitude";
+    public static final String KEY_LONGITUDE = "longitude";
+    public static final String KEY_NOTIFICATIONS_ENABLED = "notifEnabled";
+    public static final String KEY_FIRST_TIME_USE = "firstTimeUse";
+
+}
---
+++ Mobiway/app/src/main/java/ro/pub/acs/mobiway/rest/RestClient.java
@@ -0,0 +1,28 @@
+package ro.pub.acs.mobiway.rest;
+
+import com.squareup.okhttp.OkHttpClient;
+
+import retrofit.RestAdapter;
+import retrofit.client.OkClient;
+import ro.pub.acs.mobiway.general.Constants;
+import ro.pub.acs.mobiway.rest.service.MobiwayService;
+import ro.pub.acs.mobiway.rest.service.SessionRequestInterceptor;
+
+public class RestClient {
+    private MobiwayService apiService;
+
+    public RestClient() {
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .setEndpoint(Constants.SERVICES_URL)
+                .setRequestInterceptor(new SessionRequestInterceptor())
+                .setClient(new OkClient(new OkHttpClient()))
+                .build();
+
+        apiService = restAdapter.create(MobiwayService.class);
+    }
+
+    public MobiwayService getApiService() {
+        return apiService;
+    }
+}
